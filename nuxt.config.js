84edbbc9f187ca4e219fa14296845f987139f8ae
MilttO0nMss <grountish@gmail.com>
2021-01-24 14:30:06 +0100

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,33 @@
 export default {
-
 	// Global page headers (https://go.nuxtjs.dev/config-head)
 	head: {
-
 		title: 'NTT-Template',
 
 		meta: [
-
 			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			{
-
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{
 				hid: 'description',
 				name: 'description',
 				content: `
 							A Template for Nuxt.js + Tailwind CSS + Three.js Projects.
-						`
-
-			}
-
+						`,
+			},
 		],
 
-		link: [
-
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-
-		]
-
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
 	},
-
+	
 	// Global CSS (https://go.nuxtjs.dev/config-css)
-	css: [
-	],
+	css: [],
 
 	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
 	plugins: [
-
-		{src: '@/plugins/vue-tippy', ssr: true},
-		{src: '@/plugins/v-click-outside', ssr: true},
-
+		{ src: '@/plugins/vue-tippy', ssr: true },
+		{ src: '@/plugins/v-click-outside', ssr: true },
 	],
 
 	// Auto import components (https://go.nuxtjs.dev/config-components)
@@ -46,34 +35,29 @@ export default {
 
 	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
 	buildModules: [
-
 		// https://go.nuxtjs.dev/tailwindcss
 		'@nuxtjs/tailwindcss',
-		'nuxt-gsap-module',
-		'nuxt-compress'
-
+		'nuxt-compress',
 	],
 
 	// Modules (https://go.nuxtjs.dev/config-modules)
 	modules: [
-
 		// https://go.nuxtjs.dev/axios
 		'@nuxtjs/axios',
 		// https://go.nuxtjs.dev/pwa
 		'@nuxtjs/pwa',
 
 		[
-			"nuxt-compress",
+			'nuxt-compress',
 			{
 				gzip: {
-					cache: true
+					cache: true,
 				},
 				brotli: {
-					threshold: 10240
-				}
-			}
-		]
-
+					threshold: 10240,
+				},
+			},
+		],
 	],
 
 	// Axios module configuration (https://go.nuxtjs.dev/config-axios)
@@ -81,77 +65,39 @@ export default {
 
 	// Build Configuration (https://go.nuxtjs.dev/config-build)
 	build: {
+		transpile: ['gsap']
 	},
 
 	// GSAP
-	gsap: {
-
-		extraPlugins: {
-
-			cssRule: false,
-			draggable: false,
-			easel: false,
-			motionPath: false,
-			pixi: false,
-			text: true,
-			scrollTo: false,
-			scrollTrigger: false
-
-		},
-
-		extraEases: {
-
-			expoScaleEase: false,
-			roughEase: false,
-			slowMo: false
-
-		}
-
-	},
 
 	// Global page transition
 	pageTransition: {
-
 		name: 'page',
 		mode: 'out-in',
 		css: false,
 
 		beforeEnter(el) {
-
 			this.$gsap.set(el, {
-
-				opacity: 0
-
-			});
-
+				opacity: 0,
+			})
 		},
 
 		enter(el, done) {
-
 			this.$gsap.to(el, {
-
 				opacity: 1,
 				duration: 0.3,
 				ease: 'power2.inOut',
-				onComplete: done
-
-			});
-
+				onComplete: done,
+			})
 		},
 
 		leave(el, done) {
-
 			this.$gsap.to(el, {
-
 				opacity: 0,
 				duration: 0.3,
 				ease: 'power2.inOut',
-				onComplete: done
-
-			});
-
-		}
-
-	}
-
+				onComplete: done,
+			})
+		},
+	},
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,182 +1,146 @@
 <template>
-<div class="relative w-screen h-screen flex flex-col overflow-y-hidden overflow-x-hidden">
+  <div>
+    <section class="bg-green-300">
+      <div class="description panel blue bg-green-300">
+        <div class="p-20">
+          <h1>Scrub animations</h1>
+          <p>
+            A ScrollTrigger with <code>scrub: true</code> links the scroll
+            position to an animation's progress, making the scroll bar act like
+            a scrubber while it's between the ScrollTrigger's
+            <code>start</code> and <code>end</code>.
+          </p>
+          <div class="scroll-down">
+            Scroll down
+            <div class="arrow"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="panel bg-red-400 red">
+      <p>
+        <span class="line line-1"></span>This line's animation will begin when
+        it enters the viewport and finish when its top edge hits the top of the
+        viewport, staying perfectly in sync with the scrollbar because it has
+        <code>scrub:&nbsp;true</code>
+      </p>
+    </section>
 
-	<div class="w-full h-full overflow-y-auto flex flex-col">
+    <section class="panel bg-orange-400 orange">
+      <p>
+        <span class="line line-2"></span>This orange panel gets pinned when its
+        top edge hits the top of the viewport, then the line's animation is
+        linked with the scroll position until it has traveled 100% of the
+        viewport's height (<code>end: "+=100%"</code>), then the orange panel is
+        unpinned and normal scrolling resumes. Padding is added automatically to
+        push the rest of the content down so that it catches up with the scroll
+        when it unpins. You can set <code>pinSpacing: false</code> to prevent
+        that if you prefer.
+      </p>
+    </section>
 
-		<header class="w-full h-18 px-2 flex-shrink-0 bg-gray-900 flexcontainer">
+    <section class="panel bg-purple-400 purple">
+      <p>
+        <span class="line line-3"></span>This panel gets pinned in a similar
+        way, and has a more involved animation that's wrapped in a timeline,
+        fading the background color and animating the transforms of the
+        paragraph in addition to the line, all synced with the scroll position
+        perfectly.
+      </p>
+    </section>
 
-			<nuxt-link to="/resource" class="relative w-auto min-w-64 flex-shrink-0 text-xl sm:text-2xl md:text-3xl text-gray-300 font-semibold flex">
-				<div id="ntt-text"></div>
-				<div class="ml-2 text-pink-500">
-					<span id="text"></span>
-					<span id="cursor">_</span>
-				</div>
-			</nuxt-link>
-
-			<div class="w-auto ml-auto mr-0 sm:mr-8 h-full flexcontainer select-none">
-
-				<div @click="popingUp = true" class="btn w-22 h-10 mx-1 rounded-full border-2 border-teal-600 bg-teal-600 bg-opacity-30 hover:bg-opacity-80 text-xl cursor-pointer text-gray-400 flexcontainer" content="Pop up" v-tippy="{arrow: true, placement: 'top'}">Three.js</div>
-
-			</div>
-
-		</header>
-
-		<main class="w-full flex-1 min-h-18 flex">
-
-			<!-- Main -->
-			<div class="w-full h-full flex flex-col overflow-hidden">
-
-				<div class="scene w-full h-full">
-					<Scene/>
-				</div>
-
-				<div>
-					<svg class="git absolute bottom-16 transform scale-300 text-pink-600" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path
-							clip-rule="evenodd"
-							fill-rule="evenodd"
-							fill="currentColor"
-							d="M7.976 0A7.977 7.977 0 000 7.976c0 3.522 2.3 6.507 5.431 7.584.392.049.538-.196.538-.392v-1.37c-2.201.49-2.69-1.076-2.69-1.076-.343-.93-.881-1.175-.881-1.175-.734-.489.048-.489.048-.489.783.049 1.224.832 1.224.832.734 1.223 1.859.88 2.3.685.048-.538.293-.88.489-1.076-1.762-.196-3.621-.881-3.621-3.964 0-.88.293-1.566.832-2.153-.05-.147-.343-.978.098-2.055 0 0 .685-.196 2.201.832.636-.196 1.322-.245 2.007-.245s1.37.098 2.006.245c1.517-1.027 2.202-.832 2.202-.832.44 1.077.146 1.908.097 2.104.49.538.832 1.272.832 2.153 0 3.083-1.86 3.719-3.62 3.915.293.244.538.733.538 1.467v2.202c0 .196.146.44.538.392A7.984 7.984 0 0016 7.976C15.951 3.572 12.38 0 7.976 0z"
-						/>
-					</svg>
-
-					<svg class="cube absolute top-16 left-4 transform scale-300 text-teal-600" width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path
-							d="M13.51 4l-5-3h-1l-5 3-.49.86v6l.49.85 5 3h1l5-3 .49-.85v-6L13.51 4zm-6 9.56l-4.5-2.7V5.7l4.5 2.45v5.41zM3.27 4.7l4.74-2.84 4.74 2.84-4.74 2.59L3.27 4.7zm9.74 6.16l-4.5 2.7V8.15l4.5-2.45v5.16z"
-							fill="currentColor"
-						/>
-					</svg>
-				</div>
-
-
-			</div>
-
-		</main>
-
-	</div>
-
-	<!-- Pop Up -->
-	<div v-if="popingUp" class="absolute inset-0 bg-gray-900 bg-opacity-75">
-		<div class="w-full h-full flexcontainer">
-			<PopUp @externalClick="popingUp = $event" />
-		</div>
-	</div>
-
-</div>
+    <section class="panel gray">DONE!</section>
+  </div>
 </template>
 
 <script>
+// typical import
+import gsap from 'gsap'
+
+// or get other plugins:
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
 export default {
+  name: 'Home',
 
-	name: "Home",
+  data() {
+    return {}
+  },
 
-	data() {
+  methods: {
+    initAnimation() {
+      gsap.registerPlugin(ScrollTrigger)
 
-		return {
+      // --- RED PANEL ---
+      gsap.from('.line-1', {
+        scrollTrigger: {
+          trigger: '.line-1',
+          scrub: true,
+          start: 'top bottom',
+          end: 'top top',
+        },
+        scaleX: 0,
+        transformOrigin: 'left center',
+        ease: 'none',
+      })
 
-			popingUp: false,
-			showComponent: false,
+      // --- ORANGE PANEL ---
+      gsap.from('.line-2', {
+        scrollTrigger: {
+          trigger: '.orange',
+          scrub: true,
+          pin: true,
+          start: 'top top',
+          end: '+=100%',
+        },
+        scaleX: 0,
+        autoAlpha: 0,
+        transformOrigin: 'left center',
+        ease: 'none',
+      })
 
-		}
+      // --- PURPLE/GREEN PANEL ---
+      var tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.purple',
+          scrub: true,
+          pin: true,
+          start: 'top top',
+          end: '+=100%',
+        },
+      })
 
-	},
+      tl.from('.purple p', {
+        scale: 0.3,
+        rotation: 45,
+        autoAlpha: 0,
+        ease: 'power2',
+      })
+        .from(
+          '.line-3',
+          { scaleX: 0, transformOrigin: 'left center', ease: 'none' },
+          0
+        )
+        .to('.purple', { backgroundColor: '#28a92b' }, 0)
+    },
+  },
 
-	methods: {
-
-		initAnimation(){
-
-			const gsap = this.$gsap;
-
-			const words = ['Demo.', 'Showcase.', 'Examples.'];
-
-			let cursor = gsap.to('#cursor', {
-
-				opacity: 0,
-				duration: 0.5,
-				ease: "power2",
-				repeat: -1
-
-			});
-
-			let mainTimeline = gsap.timeline({repeat: -1, delay: 1.5});
-
-			words.forEach( (word) => {
-
-				let tl = gsap.timeline({repeat: 1, yoyo: true});
-				tl.to('#text', {duration: 1, text: word});
-
-				mainTimeline.add(tl);
-
-			});
-
-		},
-
-		animateNttText() {
-
-			this.$gsap.to('#ntt-text', {
-
-				duration: 1,
-				text: 'NTT-Template',
-				ease: "power2",
-				delay: 0.3
-
-			});
-
-		},
-
-		animateSvg() {
-
-			const gsap = this.$gsap;
-
-			const tl = gsap.timeline({});
-
-			tl.to('.cube', {
-
-				y: '90vh',
-				rotation: 360,
-				duration: 6,
-				ease: 'power3.inOut',
-				repeat: -1,
-				yoyo: true
-
-			}).fromTo('.git', {
-
-				x: '10vw',
-				rotation: 360,
-				duration: 3,
-				ease: 'back.inOut(3)',
-				repeat: -1,
-				yoyo: true,
-
-			}, {
-
-				x: '80vw',
-				rotation: 360,
-				duration: 3,
-				ease: 'back.inOut(3)',
-				repeat: -1,
-				yoyo: true,
-
-			}, 0)
-
-		}
-
-	},
-
-	mounted () {
-
-		const gsap = this.$gsap;
-		const TextPlugin = this.$TextPlugin;
-
-		gsap.registerPlugin(TextPlugin);
-
-		this.initAnimation();
-		this.animateNttText();
-		this.animateSvg();
-
-	},
-
+  mounted() {
+    this.initAnimation()
+  },
 }
-
 </script>
+<style scoped>
+section {
+  height: 100vh;
+}
+.line {
+  width: 100%;
+  max-width: 800px;
+  height: 8px;
+  margin: 0 0 10px 0;
+  position: relative;
+  display: inline-block;
+  background-color: rgba(255, 255, 255, 1);
+}
+</style>
